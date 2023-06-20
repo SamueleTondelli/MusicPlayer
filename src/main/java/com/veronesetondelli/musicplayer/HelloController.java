2547b92f2ea5e46fc83ce297b54458731bdc91bf
Samuele Tondelli <samueletondelli92@gmail.com>
2023-06-20 12:08:41 +0200

    updated AudioPlayer and added Playlist

--- src/main/java/com/veronesetondelli/musicplayer/HelloController.java
+++ src/main/java/com/veronesetondelli/musicplayer/HelloController.java
@@ -1,19 +1,53 @@
 package com.veronesetondelli.musicplayer;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-    AudioPlayer audioPlayer = new AudioPlayer();
     @FXML
-    protected void onHelloButtonClick() {
+    private Button btn;
+    Playlist playlist;
+    @FXML
+    protected void onLoadButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
-        if (!audioPlayer.isPlaying) {
-            audioPlayer.loadAudioFile("C:\\Users\\samue\\IdeaProjects\\testClip\\src\\main\\resources\\chepalle.wav");
-            Thread t = new Thread(audioPlayer);
+        if (playlist == null) {
+            playlist = new Playlist();
+            playlist.addSong("C:\\Users\\samue\\IdeaProjects\\MusicPlayer\\src\\main\\resources\\com\\veronesetondelli" + "\\musicplayer\\chepalle.wav");
+            playlist.addSong("C:\\Users\\samue\\IdeaProjects\\MusicPlayer\\src\\main\\resources\\com\\veronesetondelli" + "\\musicplayer\\vecchio.wav");
+            Thread t = new Thread(playlist);
             t.start();
         }
+        else if (playlist.stop) {
+            playlist.index = 0;
+            Thread t = new Thread(playlist);
+            t.start();
+        }
+    }
+
+    @FXML
+    void onPlayButtonClick() {
+        if (playlist.stop) return;
+        if (playlist.playing) {
+            btn.setText("play");
+            playlist.pause();
+        }
+        else {
+            btn.setText("pause");
+            playlist.playCurrentIndex();
+        }
+    }
+
+    @FXML
+    void onSkipButtonPress() {
+        if (!playlist.stop) {playlist.skip = true;}
+    }
+
+    @FXML
+    void onStopButtonPress() {
+        btn.setText("pause");
+        playlist.stop = true;
     }
 }
---
+++ src/main/java/com/veronesetondelli/musicplayer/Playlist.java
@@ -0,0 +1,74 @@
+package com.veronesetondelli.musicplayer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Playlist implements Runnable{
+    List<String> songList;
+    AudioPlayer player;
+    boolean playing;
+    boolean skip;
+    boolean stop;
+    int index;
+
+    public Playlist() {
+        songList = new ArrayList<String>();
+        player = new AudioPlayer();
+    }
+
+    void addSong(String name) {
+        songList.add(name);
+    }
+
+    void removeSong(String name) {
+        songList.remove(name);
+    }
+
+    void removeSong(int index) throws IndexOutOfBoundsException{
+        songList.remove(index);
+    }
+
+    void playCurrentIndex() {
+        playing = true;
+        player.play();
+    }
+
+    void pause() {
+        playing = false;
+        player.stop();
+    }
+
+    void loadCurrentIndex() {
+        player.loadAudioFile(songList.get(index));
+    }
+
+    @Override
+    public void run() {
+        loadCurrentIndex();
+        playCurrentIndex();
+        stop = false;
+        while (true) {
+            skip = false;
+            while (!player.playHasEnded) {
+                if (stop) {
+                    playing = false;
+                    player.close();
+                    return;
+                }
+                if (skip) {
+                    player.stop();
+                    break;
+                }
+                try {
+                    Thread.sleep(100);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return;
+                }
+            }
+            index = (index + 1) % songList.size();
+            loadCurrentIndex();
+            playCurrentIndex();
+        }
+    }
+}
