befae7de5e949ad0ca4a2d08ce8a214ca1d158d7
Samuele Tondelli <samueletondelli92@gmail.com>
2023-07-04 10:10:00 +0200

    update

--- src/main/java/com/veronesetondelli/musicplayer/MusicPlayerController.java
+++ src/main/java/com/veronesetondelli/musicplayer/MusicPlayerController.java
@@ -6,15 +6,15 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,15 +28,16 @@ public class MusicPlayerController implements Runnable{
     @FXML
     private Label volumeLabel;
 
-    ArrayList<Playlist> playlist = new ArrayList<>();
-    int currentlySelectedPlaylist = 0;
-    private boolean playing = false;
-    private boolean skip = false;
-    private boolean previous = false;
-    private boolean stop = true;
-    private boolean mute = false;
-    private boolean updateProgressBar = true;
-    private boolean jump = false;
+    ObservableList<Playlist> playlistList;
+    private int currentlyPlayingPlaylist;
+    private int currentlySelectedPlaylist;
+    private boolean playing;
+    private boolean skip;
+    private boolean previous;
+    private boolean stop;
+    private boolean mute;
+    private boolean updateProgressBar;
+    private boolean jump;
     private double currVol;
     @FXML
     private Slider volumeSlider;
@@ -55,8 +56,19 @@ public class MusicPlayerController implements Runnable{
 
     @FXML
     void initialize() {
+        currentlyPlayingPlaylist = -1;
+        playing = false;
+        skip = false;
+        previous = false;
+        stop = true;
+        mute = false;
+        updateProgressBar = true;
+        jump = false;
+
+        playlistList = FXCollections.observableArrayList();
+
         songListView.getSelectionModel().selectedIndexProperty().addListener((observable, oldValue, newValue) -> {
-            playlist.get(currentlySelectedPlaylist).index = newValue.intValue();
+            getPlayingPlaylist().index = newValue.intValue();
             jump = true;
         });
 
@@ -65,23 +77,24 @@ public class MusicPlayerController implements Runnable{
         volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                playlist.get(currentlySelectedPlaylist).setVolume(volumeSlider.getValue() * 0.01);
+                getPlayingPlaylist().setVolume(volumeSlider.getValue() * 0.01);
             }
         });
     }
     @FXML
     protected void onLoadButtonClick() {
-        if (playlist.size() == 0) {
+        if (playlistList.size() == 0) {
             Playlist p = new Playlist("p1");
-            p.addSong("C:\\Users\\veron\\IdeaProjects\\MusicPlayer\\src\\main\\resources\\com\\veronesetondelli" + "\\musicplayer\\chepalle.wav");
-            p.addSong("C:\\Users\\veron\\IdeaProjects\\MusicPlayer\\src\\main\\resources\\com\\veronesetondelli" + "\\musicplayer\\vecchio.wav");
-            playlist.add(p);
+            p.addSong("C:\\Users\\samue\\Desktop\\songs\\chepalle.wav");
+            p.addSong("C:\\Users\\samue\\Desktop\\songs\\vecchio.wav");
+            playlistList.add(p);
+            currentlyPlayingPlaylist = 0;
             //currentPlaylistLabel.setText(p.name);
             Thread t = new Thread(this);
             t.start();
         }
         else if (stop) {
-            playlist.get(currentlySelectedPlaylist).index = 0;
+            getPlayingPlaylist().index = 0;
             Thread t = new Thread(this);
             t.start();
         }
@@ -90,12 +103,12 @@ public class MusicPlayerController implements Runnable{
     void onPlayButtonClick() {
         if (stop) return;
         if (playing) {
-            playlist.get(currentlySelectedPlaylist).pause();
+            getPlayingPlaylist().pause();
             playing = false;
         }
         else {
             //btn.setText("pause");
-            playlist.get(currentlySelectedPlaylist).playCurrentIndex();
+            getPlayingPlaylist().playCurrentIndex();
             playing = true;
         }
     }
@@ -107,7 +120,7 @@ public class MusicPlayerController implements Runnable{
     void onPreviousButtonPress() {
         if (!stop) {
             previous = true;
-            playlist.get(currentlySelectedPlaylist).previousSong();
+            getPlayingPlaylist().previousSong();
         }
     }
     @FXML
@@ -132,7 +145,7 @@ public class MusicPlayerController implements Runnable{
 
             Optional<ButtonType> clickedButton = dialog.showAndWait();
             if (clickedButton.orElse(ButtonType.CANCEL) == ButtonType.OK) {
-                playlist.add(controller.getPlaylist());
+                playlistList.add(controller.getPlaylist());
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -155,7 +168,7 @@ public class MusicPlayerController implements Runnable{
 
             Optional<ButtonType> clickedButton = dialog.showAndWait();
             if (clickedButton.orElse(ButtonType.CANCEL) == ButtonType.OK) {
-                playlist.add(controller.getPlaylist());
+                playlistList.add(controller.getPlaylist());
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -163,9 +176,9 @@ public class MusicPlayerController implements Runnable{
     }
     @FXML
     void onNextPlaylistButtonClick() {
-        currentlySelectedPlaylist = (currentlySelectedPlaylist + 1) % playlist.size();
-        currentPlaylistLabel.setText(playlist.get(currentlySelectedPlaylist).name);
-        songListView.setItems(playlist.get(currentlySelectedPlaylist).getSongNames());
+        currentlyPlayingPlaylist = (currentlyPlayingPlaylist + 1) % playlistList.size();
+        currentPlaylistLabel.setText(getPlayingPlaylist().name);
+        songListView.setItems(getPlayingPlaylist().getSongNames());
     }
     @FXML
     void handleAddSongClick() {
@@ -174,7 +187,7 @@ public class MusicPlayerController implements Runnable{
             fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Audio files (*.wav, *.mp3)", "*.wav", "*.mp3"));
 
             File file = fileChooser.showOpenDialog(null);
-            playlist.get(currentlySelectedPlaylist).addSong(file.getAbsolutePath());
+            getSelectedPlaylist().addSong(file.getAbsolutePath());
         } catch (Exception e) {
             new Alert(Alert.AlertType.ERROR, "Could not load song").showAndWait();
         }
@@ -192,7 +205,7 @@ public class MusicPlayerController implements Runnable{
                 List<String> songs = mapper.readValue(file, new TypeReference<>() {});
                 Playlist p = new Playlist(file.getName().substring(0, file.getName().length() - 5));
                 p.addSongs(songs);
-                playlist.add(p);
+                playlistList.add(p);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -208,7 +221,7 @@ public class MusicPlayerController implements Runnable{
             if (file != null) {
                 ObjectMapper mapper = new ObjectMapper();
                 mapper.registerModule(new JavaTimeModule());
-                mapper.writerWithDefaultPrettyPrinter().writeValue(file, playlist.get(currentlySelectedPlaylist).songList);
+                mapper.writerWithDefaultPrettyPrinter().writeValue(file, getPlayingPlaylist().songList);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -229,49 +242,44 @@ public class MusicPlayerController implements Runnable{
     }
     @FXML
     void handleSongProgressChange() {
-        playlist.get(currentlySelectedPlaylist).setCurrentSongProgress(songProgressSlider.getValue());
+        getPlayingPlaylist().setCurrentSongProgress(songProgressSlider.getValue());
         updateProgressBar = true;
     }
     @FXML
     void onSongProgressSliderPress() {
         updateProgressBar = false;
     }
+    Playlist getPlayingPlaylist() { return playlistList.get(currentlyPlayingPlaylist); }
+    Playlist getSelectedPlaylist() { return playlistList.get(currentlySelectedPlaylist); }
     @Override
     public void run() {
-        playlist.get(currentlySelectedPlaylist).loadCurrentIndex();
-        playlist.get(currentlySelectedPlaylist).playCurrentIndex();
-        stop = false;
-        playing = true;
         while (true) {
-            playlist.get(currentlySelectedPlaylist).setVolume(volumeSlider.getValue() * 0.01);
+            getPlayingPlaylist().loadCurrentIndex();
+            getPlayingPlaylist().playCurrentIndex();
+            stop = false;
+            playing = true;
             skip = false;
             previous = false;
+            getPlayingPlaylist().setVolume(volumeSlider.getValue() * 0.01);
 
-            Platform.runLater(new Runnable() {
-                public void run() {
-                    currentSongLabel.setText(playlist.get(currentlySelectedPlaylist).getCurrentSongName());
-                }
-            });
+            Platform.runLater(() -> currentSongLabel.setText(getPlayingPlaylist().getCurrentSongName()));
 
-            while (playlist.get(currentlySelectedPlaylist).player.isPlaying()) {
+            while (getPlayingPlaylist().player.isPlaying()) {
                 if (updateProgressBar) {
-                    Platform.runLater(new Runnable() {
-                        @Override
-                        public void run() {
-                            songProgressSlider.setValue(playlist.get(currentlySelectedPlaylist).getCurrentSongProgress());
-                            secondsLabel.setText(Double.toString(playlist.get(currentlySelectedPlaylist).player.getPlayingTimeSeconds()));
+                    Platform.runLater(() -> {
+                            songProgressSlider.setValue(getPlayingPlaylist().getCurrentSongProgress());
+                            secondsLabel.setText(Double.toString(getPlayingPlaylist().player.getPlayingTimeSeconds()));
                             volumeLabel.setText(Long.toString(Math.round(volumeSlider.getValue())));
-                            currentSongProgress.setText(Double.toString(playlist.get(currentlySelectedPlaylist).getCurrentSongProgress()));
-                        }
-                    });
+                            currentSongProgress.setText(Double.toString(getPlayingPlaylist().getCurrentSongProgress()));
+                        });
                 }
                 if (stop) {
                     playing = false;
-                    playlist.get(currentlySelectedPlaylist).player.stop();
+                    getPlayingPlaylist().player.stop();
                     return;
                 }
                 if (skip || jump || previous) {
-                    playlist.get(currentlySelectedPlaylist).player.stop();
+                    getPlayingPlaylist().player.stop();
                     break;
                 }
 
@@ -282,15 +290,10 @@ public class MusicPlayerController implements Runnable{
                     return;
                 }
             }
+            if (getPlayingPlaylist().getPlaylistLength() == 0) return;
             if (!jump) {
-                playlist.get(currentlySelectedPlaylist).nextSong();
-            } else {
-                jump = false;
+                getPlayingPlaylist().nextSong();
             }
-
-
-            playlist.get(currentlySelectedPlaylist).loadCurrentIndex();
-            playlist.get(currentlySelectedPlaylist).playCurrentIndex();
         }
     }
 }
