50a5724e8d9720f1ca7414f8c760ec3b202f7339
Samuele Tondelli <samueletondelli92@gmail.com>
2023-07-07 16:11:37 +0200

    Added entry point and manifest

---
+++ src/main/java/com/veronesetondelli/musicplayer/EntryPoint.java
@@ -0,0 +1,10 @@
+package com.veronesetondelli.musicplayer;
+
+/**
+ * Entry point for the .jar executable
+ */
+public class EntryPoint {
+    public static void main(String[] args) {
+        MusicPlayerApplication.main(args);
+    }
+}
